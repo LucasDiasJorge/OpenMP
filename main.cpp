e5a2b5ee74ef561fb1ef8133dcc747c2b013ab89
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-06-24 21:34:27 -0300

    Setting threads from argc

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <omp.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    omp_set_num_threads(4);
+    if(argv[1] == NULL){
+        std::cout << "Specify threads quantity after ./executable" << std::endl;
+        return -1;
+    }
+
+    int threadNumbers = atoi(argv[1]);
+
+    omp_set_num_threads(threadNumbers);
 
     std::cout << "------ Without critical part ------" << std::endl;
 
     #pragma omp parallel
     {
         int threadID = omp_get_thread_num();
-        std::cout << "Hello from thread" << threadID << std::endl;
+        std::cout << "Hello from thread " << threadID << std::endl;
     }
 
     std::cout << "------ With critical part ------" << std::endl;
